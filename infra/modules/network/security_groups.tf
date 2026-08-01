889ae067fa7d4627fe02fe772252c7c6c800a927
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2026-08-01 12:18:39 -0300

    feat: implement security group for ecs egress

--- infra/modules/network/security_groups.tf
+++ infra/modules/network/security_groups.tf
@@ -78,3 +78,11 @@ resource "aws_vpc_security_group_egress_rule" "allow_egress_to_rds" {
   to_port                      = 5432
   ip_protocol                  = "tcp"
 }
+
+resource "aws_vpc_security_group_egress_rule" "allow_egress_to_vpc_endpoints" {
+  security_group_id            = aws_security_group.ecs_sg.id
+  referenced_security_group_id = aws_security_group.vpc_endpoints_sg.id
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+}
