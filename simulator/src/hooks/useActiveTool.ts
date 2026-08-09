a573ba589a075d0c9a866011ea3838205083ba43
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2026-08-09 19:58:15 -0300

    chore: move the simulator to its own repository

--- simulator/src/hooks/useActiveTool.ts
+++
@@ -1,8 +0,0 @@
-import { useMemo } from 'react'
-import { findTool, type ToolDefinition } from '../canvas/tools'
-import { useToolStore } from '../store/useToolStore'
-
-export function useActiveTool(): ToolDefinition {
-  const activeToolId = useToolStore((state) => state.activeToolId)
-  return useMemo(() => findTool(activeToolId), [activeToolId])
-}
